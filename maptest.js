4b860338c4652fcabc31789174047b835db5f19c
dr-jts <mtnclimb@gmail.com>
2017-01-20 13:12:33 -0800

    Only show one popup at a time

--- maptest.js
+++ maptest.js
@@ -159,7 +159,7 @@ MapTest.prototype.initInfoCtl = function(ov) {
                         true
                     );
                     pop.autoSize = false;
-                    this.map.addPopup(pop);
+                    this.map.addPopup(pop, true);
                 }
             }
            });
@@ -212,6 +212,88 @@ MapTest.prototype.loadConfig = function(configStr) {
 		ov.addLayers(layers);
 	}
 }
+// recursive descent parser for config
+ConfigParser = function( mapTest ) {
+	this.mapTest = mapTest;
+}
+ConfigParser.prototype.parse = function( config ) {
+	var lines = configStr.split(/[\n]/);
+	while (true) {
+		var ov = this.parseOverlay( lines );
+		if (ov) {
+			this.mapTest.addOverlay( ov );
+		}
+		else {
+			return;
+		}
+	}
+}
+ConfigParser.prototype.parseOverlay = function( lines ) {
+	if (lines.length == 0) return null;
+	var url = lines[0];
+	if (! Lexer.isURL( url )) return null;
+	lines.shift();
+	var ov = {
+		url: url,
+		metadataURL: ''
+	};
+	this.parseOverlayParams( lines, ov );
+	this.parseLayers( lines, ov );
+	return ov;
+}
+ConfigParser.prototype.parseOverlayParams = function( lines, ov ) {
+	while (true) {
+		var line = scanLine(lines);
+		if (line == null) return;
+		var isParam = false;
+		if (Lexer.isTag(MapTest.constants.META_URL, line)) {
+			ov.metadataURL = Lexer.taggedValue(line);
+			isParam = true;
+		}
+		if (Lexer.isTag(MapTest.constants.PARAM, line)) {
+			var nv = Lexer.taggedParam(line);
+			ov.param = $.extend({}, ov.param, nv);
+			isParam = true;
+		}
+		// done params
+		if (! isParam) return;
+		lines.shift();
+	}
+}
+ConfigParser.prototype.parseLayers = function( lines, ov ) {
+	while (true) {
+		var line = scanLine(lines);
+		if (line == null) return;
+		if (Lexer.isURL(MapTest.constants.META_URL, line)) {
+			ov.metadataURL = Lexer.taggedValue(line);
+			isParam = true;
+		}
+		if (Lexer.isTag(MapTest.constants.PARAM, line)) {
+			var nv = Lexer.taggedParam(line);
+			ov.param = $.extend({}, ov.param, nv);
+			isParam = true;
+		}
+		// done params
+		if (! isParam) return;
+		lines.shift();
+	}
+}
+function scanLine( lines ) {
+	while (true) {
+		if (lines.length <= 0) return null;
+		var line = shimTrim(lines[0]);
+		if (line.length < 1) {
+			lines.shift();
+			continue;
+		}
+		if (line.startsWith('#')) {
+			lines.shift();
+			continue;
+		}
+		return line;
+	}
+}
+
 var Lexer = {
 	isURL: function (s) {
 		if (s.indexOf('http:') == 0) return true;
